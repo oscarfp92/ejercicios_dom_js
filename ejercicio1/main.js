1277c49266ffc77b25a05e67ad6bb3c55cacf278
Oscarfp <oscarfp1992@gmail.com>
2024-09-06 19:06:53 +0200

    ok

---
+++ ejercicio1/main.js
@@ -0,0 +1,6 @@
+//1.1 Usa querySelector para mostrar por consola el botón con la clase .showme
+// Seleccionar el botón con la clase .showme
+const boton = document.querySelector('.showme');
+
+// Mostrar el botón en la consola
+console.log(boton);
