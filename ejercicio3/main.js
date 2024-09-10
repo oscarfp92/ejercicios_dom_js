8373357d77be536c4c9e17847472289e2bfadca4
Oscarfp <oscarfp1992@gmail.com>
2024-09-10 16:12:31 +0200

    ejercicio 3 ok

--- ejercicio3/main.js
+++ ejercicio3/main.js
@@ -8,9 +8,11 @@ for (const country of countries) {
 } 
 document.body.appendChild (countryList);
 
+
 // 3.2 Elimina el elemento que tenga la clase .fn-remove-me.
 const element = document.querySelector (".fn-remove-me");
 element.remove();
+
 
 // 3.3 Utiliza el array para crear dinámicamente una lista ul > li de elementos en el div de html con el atributo data-function="printHere".
 const cars = ['Mazda 6', 'Ford fiesta', 'Audi A4', 'Toyota corola'];
@@ -22,6 +24,7 @@ for (const car of cars) {
 }
 const box = document.querySelector ("[data-function=printHere]");
 box.appendChild(carsList);
+
 
 // 3.4  Crea dinámicamente en el html una serie de divs que contenga un elemento h4 para el título y otro elemento img para la imagen.
 const countries2 = [
@@ -35,25 +38,54 @@ const countries2 = [
 for (const country2 of countries2) {
   const boxes = document.createElement ("div");
   const titleBox = document.createElement ("h4");
-  const image = document.createElement ("img")
+  const image = document.createElement ("img");
   titleBox.textContent = country2.title;
-  image.textContent = country2.imgUrl;
+  image.src = country2.imgUrl;
   boxes.appendChild (titleBox);
   boxes.appendChild (image);
   document.body.appendChild (boxes);
 }
 
+
 // 3.5 Basándote en el ejercicio anterior. Crea un botón que elimine el último elemento de la serie de divs.
 const boton = document.createElement ("button");
-boton.textContent = "elimina el último div"
+boton.textContent = "Elimina el último div";
 document.body.appendChild (boton);
 
 const lastDiv = document.querySelector ("div:last-of-type");
 lastDiv.classList.add ("ultimo");
 
-const ultimoDiv = document.querySelector (".ultimo")
+const ultimoDiv = document.querySelector (".ultimo");
 boton.addEventListener ("click", function () {
-  ultimoDiv.remove()
+  ultimoDiv.remove();
 });
 
+
 // 3.6 Basándote en el ejercicio anterior. Crea un botón para cada uno de los divs que elimine ese mismo elemento del html.
+const countries = [
+	{title: 'Random title', imgUrl: 'https://picsum.photos/300/200?random=1'}, 
+	{title: 'Random title', imgUrl: 'https://picsum.photos/300/200?random=2'},
+	{title: 'Random title', imgUrl: 'https://picsum.photos/300/200?random=3'},
+	{title: 'Random title', imgUrl: 'https://picsum.photos/300/200?random=4'},
+	{title: 'Random title', imgUrl: 'https://picsum.photos/300/200?random=5'}
+];
+
+for (country of countries) {
+  const boxes = document.createElement ("div");
+  const title = document.createElement ("h4");
+  const image = document.createElement ("img");
+  const button = document.createElement ("button");
+
+  title.textContent = country.title;
+  image.src = country.imgUrl;
+  button.textContent = "Elimíname";
+
+  button.addEventListener("click", function () {
+    boxes.remove();
+  })
+
+boxes.appendChild (title);
+boxes.appendChild (image);
+boxes.appendChild (button);
+document.body.appendChild(boxes);
+}
